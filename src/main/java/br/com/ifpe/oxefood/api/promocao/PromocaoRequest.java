ab7be3a8202a13821f4ac7a2ecc330036762acc8
aluno <ibds@discente.ifpe.edu.br>
2024-07-16 19:02:33 -0300

    configurando o projeto

--- src/main/java/br/com/ifpe/oxefood/api/promocao/PromocaoRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/promocao/PromocaoRequest.java
@@ -2,6 +2,8 @@ package br.com.ifpe.oxefood.api.promocao;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.ifpe.oxefood.modelo.promocao.Promocao;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,8 +18,10 @@ public class PromocaoRequest {
 
     private String titulo;
 
+      @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataInicio;
 
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFim;
 
     private String regra;
